cf09aee0ef5dc593e57471aca7132f067108ed22
Forrest Grover <fjgrover@hotmail.com>
2020-01-07 13:37:46 -0500

    data is queried from server every 5 seconds

--- app.ts
+++ app.ts
@@ -2,8 +2,6 @@ import {
     OPCUAClient,
     MessageSecurityMode,
     SecurityPolicy,
-    stringToQualifiedName,
-    TranslateBrowsePathsToNodeIdsRequest,
     AttributeIds,
 } from 'node-opcua';
 
@@ -24,8 +22,8 @@ const client = OPCUAClient.create( options );
 const endpointUrl = "opc.tcp://192.168.5.99:4840";
 
 async function main() {
-	
-	async function timeout( ms: number ) {
+
+    async function timeout( ms: number ) {
 		return new Promise( resolve => setTimeout( resolve, ms ) );
     };
 
@@ -46,20 +44,22 @@ async function main() {
             nodesToRead.push( { nodeId: element.nodeId.toString(), attributeId: AttributeIds.Value } );
         });
 
-        const dataValue =  await session.readVariableValue(nodesToRead);
+        while ( true ) {
+            const dataValue =  await session.readVariableValue( nodesToRead );
 
-        for ( var i = 0; i < nodesToRead.length; i++ ) {
-            deviceVars[i].Value = dataValue[i].value.value;
+            for ( var i = 0; i < nodesToRead.length; i++ ) {
+                deviceVars[ i ].Value = dataValue[ i ].value.value;
+            }
+            
+            // console.log( deviceVars );
+            const json = JSON.stringify( deviceVars );
+            console.log( json );
+            
+            await timeout( 5000 );
         }
-        
-        console.log( deviceVars );
-        const json = JSON.stringify(deviceVars);
-
-        await session.close();
+    } catch ( err ) {
+        console.log( err );
         await client.disconnect();
-
-    } catch (err) {
-        console.log(err);
     }
 }
 
