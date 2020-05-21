d62a3ded2657a0d9075cad289038a5f72a1aa6d1
Forrest Grover <fjgrover@hotmail.com>
2020-05-21 15:45:28 -0400

    abstracted artifacts to .env file

--- app.ts
+++ app.ts
@@ -5,13 +5,15 @@ import {
     AttributeIds,
 } from 'node-opcua';
 
+require( 'dotenv' ).config();
+
 const connectionStrategy = {
     initialDelay: 1000,
     maxRetry: 2
 };
 
 const options = {
-    applicationName: "Opcua-Client",
+    applicationName: 'OPCUA-Client',
     connectionStrategy: connectionStrategy,
     securityMode: MessageSecurityMode.None,
     securityPolicy: SecurityPolicy.None,
@@ -19,7 +21,7 @@ const options = {
 };
 
 const client = OPCUAClient.create( options );
-const endpointUrl = "opc.tcp://192.168.5.99:4840";
+const endpointUrl = process.env.ENDPOINT_URL;
 
 async function main() {
 
@@ -29,16 +31,15 @@ async function main() {
 
     try {
         await client.connect( endpointUrl );
-        console.log( "connected" );
+        console.log( 'connected' );
 
         const session = await client.createSession();
-        console.log( "session created" );
+        console.log( 'session created' );
 
         const nodesToRead = [];
         const deviceVars = new Array();
-        const browse = "ns=4;s=|var|p500.Application.NETWORK";
         
-        const workingDir = await session.browse( browse );
+        const workingDir = await session.browse( process.env.VARIABLE_PATH );
         workingDir.references.forEach( async element => {
             deviceVars.push( { Name: element.displayName.text } );
             nodesToRead.push( { nodeId: element.nodeId.toString(), attributeId: AttributeIds.Value } );
@@ -51,14 +52,14 @@ async function main() {
                 deviceVars[ i ].Value = dataValue[ i ].value.value;
             }
             
-            // console.log( deviceVars );
             const json = JSON.stringify( deviceVars );
             console.log( json );
             
-            await timeout( 5000 );
+            await timeout( parseInt( process.env.TIMEOUT_MS ) );
         }
     } catch ( err ) {
         console.log( err );
+    } finally {
         await client.disconnect();
     }
 }
